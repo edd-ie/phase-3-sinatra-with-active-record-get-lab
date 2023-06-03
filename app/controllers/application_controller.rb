1f8d170df52158154d0e2d8fb28447b95893b407
edd-ie <ogendoeddie@gmail.com>
2023-06-03 15:45:54 +0300

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
+  get '/bakeries' do
+    bakery = Bakery.all
+    bakery.to_json
+  end
+
+  get '/baked_goods' do
+    baked_good = BakedGood.all
+    baked_good.to_json
+  end
+
+  get '/bakeries/:id' do
+    data = Bakery.find(params[:id])
+    data.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    data = BakedGood.order(price: :desc)
+    data.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    data = BakedGood.find_by(price: BakedGood.maximum("price"))
+    data.to_json
+  end
+
 
 end
--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,7 @@
 class BakedGood < ActiveRecord::Base
   # add association macro here
+  belongs_to :bakeries
+  def bakery
+    Bakery.find(self[:bakery_id])
+  end
 end
